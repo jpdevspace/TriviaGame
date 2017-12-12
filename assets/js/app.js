b09ee31cfa0f21cdb7f4e782d136b1e5ef0d4a92
jpwebdev <jjpwebdev@gmail.com>
2017-12-12 17:56:41 -0500

    HTML structured, started with the questions object in the javascript

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+(function($) {
+    // Non-DOM-ready-required code here (scope-safe)
+    $(function() {
+        //$('#startGame').modal('show');
+        const trivia = {
+            questions: [
+                {
+                    question: "Who are the original members of the Spice Girl group",
+                    options: [ "Melanie Brown, Melanie Chisholm, Emma Bunton, Geri Halliwell and Victoria Adams",
+                                "Melanie Chisholm, Melanie Bunton, Emma Brown, Geri Adams and Victoria Halliwell",
+                                "Nicole Scherzinger, Carmit Bachar, Ashley Roberts, Jessica Sutta, Melody Thornton, and Kimberly Wyatt",
+                                "AJ McLean, Howie D., Nick Carter, Kevin Richardson, and Brian Littrell",],
+                    answer: "Melanie Brown, Melanie Chisholm, Emma Bunton, Geri Halliwell and Victoria Adams",
+                    imgSrc: "../imgs/spicegirls.jpeg"
+                },
+                {
+                    question: "",
+                    options: ["","","","",],
+                    answer: "",
+                    imgSrc: ""
+                },
+                {
+                    question: "",
+                    options: ["","","","",],
+                    answer: "",
+                    imgSrc: ""
+                
+                },
+                {
+                    question: "",
+                    options: ["","","","",],
+                    answer: "",
+                    imgSrc: ""
+                },
+                {
+                    question: "",
+                    options: ["","","","",],
+                    answer: "",
+                    imgSrc: ""
+                },
+                {
+                    question: "",
+                    options: ["","","","",],
+                    answer: "",
+                    imgSrc: ""
+                },
+            ],
+        }
+    });
+
+})(jQuery); 

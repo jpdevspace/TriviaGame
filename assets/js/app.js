116548a8bc95036c0265bb8ab7aed5a98b097a6f
jpwebdev <jjpwebdev@gmail.com>
2017-12-13 18:41:59 -0500

    Lots of changes in the JavaScript file, redefined the functions to reduce the amount of code and global variables needed, now to start working in the restart button

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
     // Non-DOM-ready-required code here (scope-safe)
     $(function() {
         //$('#startGame').modal('show');
+        // IT WORKS NOW
         const trivia = {
             questionsArr: [
                 {
@@ -16,37 +17,36 @@
                     answer: "Nirvana",
                     imgSrc: "./assets/imgs/nirvana.gif"
                 },
-                {
-                    question: "In the TV show Friends: Who teaches Rachel and Phoebe about 'Unagi'?",
-                    options: ["Joey","Ross","Monica","Gunther",],
-                    answer: "Ross",
-                    imgSrc: "./assets/imgs/ross.gif"
+                // {
+                //     question: "In the TV show Friends: Who teaches Rachel and Phoebe about 'Unagi'?",
+                //     options: ["Joey","Ross","Monica","Gunther",],
+                //     answer: "Ross",
+                //     imgSrc: "./assets/imgs/ross.gif"
                 
-                },
-                {
-                    question: "Who wrote the book series Goosebumps?",
-                    options: ["George R. R. Martin","Stephen King","J.K Rowling","R.L. Stine",],
-                    answer: "R.L. Stine",
-                    imgSrc: "./assets/imgs/goosebumps.gif"
-                },
-                {
-                    question: "Complete the lyric 'You are my fire. The one desire. Believe when I say. _________'",
-                    options: ["I ate your cheesecake","I want it that way","I'm running that way", "I kicked your dear friend",],
-                    answer: "I want it that way",
-                    imgSrc: "./assets/imgs/bsb.gif"
-                },
-                {
-                    question: "Full House took place in which city?",
-                    options: ["Los Angeles","Chicago","New York City","San Francisco",],
-                    answer: "San Francisco",
-                    imgSrc: "./assets/imgs/fullhouse.gif"
-                },
+                // },
+                // {
+                //     question: "Who wrote the book series Goosebumps?",
+                //     options: ["George R. R. Martin","Stephen King","J.K Rowling","R.L. Stine",],
+                //     answer: "R.L. Stine",
+                //     imgSrc: "./assets/imgs/goosebumps.gif"
+                // },
+                // {
+                //     question: "Complete the lyric 'You are my fire. The one desire. Believe when I say. _________'",
+                //     options: ["I ate your cheesecake","I want it that way","I'm running that way", "I kicked your dear friend",],
+                //     answer: "I want it that way",
+                //     imgSrc: "./assets/imgs/bsb.gif"
+                // },
+                // {
+                //     question: "Full House took place in which city?",
+                //     options: ["Los Angeles","Chicago","New York City","San Francisco",],
+                //     answer: "San Francisco",
+                //     imgSrc: "./assets/imgs/fullhouse.gif"
+                // }
             ],
-            questionCounter: 0,
+            qCounter: 0, // Counter for the current question
             rightCount: 0,
             wrongCount: 0,
-            currentAnswer: "",
-            currentImg: "",
+            quizzInterval: 0,
 
             init() {
                 this.domCache();
@@ -60,72 +60,68 @@
             },
             eventBinding() {
                 this.$options.on('click', '.options', this.getUserGuess.bind(this));
+                this.$question.on('click', '.restart', this.restart.bind(this));
             },
             countdown() {
-                let time = 30; // Time available for each question
+                let time = 5; // Time available for each question
                 const countdown = () => {
                     time--; // Decrease time by 1
+                    if (time == 0) {
+                        this.wrongCount++;
+                        this.showMessage('Incorrect!');
+                    }
                     this.$counter.html(time);   // Display time
                 }
-                let quizzInterval = setInterval(countdown, 1000);   // Every second run countdown()
-                if (time == 0) {
-                    this.wrongGuess();    // Stop countdown at time = 0; or if click on submit  
-                }
+                this.quizzInterval = setInterval(countdown, 1000);   // Every second run countdown()
+
             },
             loadQuestions() {
-                this.countdown(); //Initialize counter
-                let currentQuestion = this.questionsArr[this.questionCounter];  // Get the current question
-                this.$question.html(currentQuestion.question); // Show that current question
+                this.countdown(); //Initialize timer
+                this.$question.html((this.questionsArr[this.qCounter]).question); // Show the current question
 
-                for (let i = 0; i < currentQuestion.options.length; i++) {
-                    this.$options.append(`<li class="options one list-group-item">${currentQuestion.options[i]}</li>`); // Show options
+                for (let i = 0; i < (this.questionsArr[this.qCounter]).options.length; i++) {    // Loop through the options array from the current question
+                    this.$options.append(`<li class="options one list-group-item">${(this.questionsArr[this.qCounter]).options[i]}</li>`); // Display all the possible options (they're stored in questionsArr > question > options)
                 }
             },
             getUserGuess(e) {
-                let btnValue = ""; // Var that will hold the user's answer
-                btnValue = e.target.innerText;  // Answer from the <li> clicked by the user
-                this.checkAnswer(btnValue); 
-            },
-            checkAnswer(btnValue) {
-                currentAnswer = this.questionsArr[this.questionCounter].answer; // Get the value of the right answer for the current question
-                currentImg = this.questionsArr[this.questionCounter].imgSrc;    // Get the image path to display correct img
-                
-                if (btnValue == currentAnswer) {    // If the user clicked the right answer
-                    this.rightAnswer();
+                let btnValue = e.target.innerText;  // Answer from the <li> clicked by the user
+
+                if(btnValue == this.questionsArr[this.qCounter].answer){    // Check if the person guessed was correct or not
+                    this.rightCount++;
+                    this.showMessage('Correct!');
+                } else {
+                    this.wrongCount++;
+                    this.showMessage('Incorrect!');
                 }
-                else {  // If user clicked the wrong answer
-                    this.wrongGuess();
-                }
-  
-            },
-            rightAnswer() {
-                this.rightCount++;  // Increase the points
-                this.$question.html(`<strong>CORRECT!</strong><br>The correct answer was ${this.currentAnswer}<br><img class="img-thumbnail" src="${this.currentImg}">`);  // Display a message
-                this.$options.empty();  // Get rid of the current options
-                this.questionCounter++; // Increment the questionCounter to later get the next question
-                console.log(this.rightCount);
-                if(this.wrongCount + this.rightCount == this.questionsArr.length) {
+                console.log(this.questionsArr.length);
+                if(this.qCounter == (this.questionsArr.length - 1)) { // Check if game over
                     this.gameOver();
                 } else {
+                    this.qCounter++; // Update the question counter to move on to the next question
                     setTimeout(this.loadQuestions.bind(this), 4000);    // Display message for 4 segs
                 }
             },
-            wrongGuess() {
-                this.wrongCount++;  // Increase the points
-                this.$question.html(`<strong>WRONG!</strong><br>The correct answer was ${this.currentAnswer}<br><img class="img-thumbnail" src="${this.currentImg}">`);  // Display a message
+            showMessage(guess) {
+                clearInterval(this.quizzInterval); // Stop the timer
                 this.$options.empty();  // Get rid of the current options
-                this.questionCounter++; // Increment the questionCounter to later get the next question
-                console.log(this.wrongCount);
-                if(this.wrongCount + this.rightCount == this.questionsArr.length) {
-                    this.gameOver();
-                } else { setTimeout(this.loadQuestions.bind(this), 4000);    // Display message for 4 segs
-                }
+                this.$question.html(`<strong>${guess}</strong><br>The correct answer was ${this.questionsArr[this.qCounter].answer}<br><img class="img-thumbnail" src="${this.questionsArr[this.qCounter].imgSrc}">`);  // Display a message   
+
             },
             gameOver(){
                 // If all the questions have been displayed
-                this.$question.html(`<h2 id="gameOver">Game Over</h2><h4>Your score <br><strong>Right: ${this.rightCount}</strong><br><strong>Wrong: ${this.wrongCount}</strong></h4>`);
-
+                this.$question.html(`
+                    <h2 id="gameOver">Game Over</h2>
+                    <h4>Your score<br>
+                        <strong>Right: ${this.rightCount}</strong><br>
+                        <strong>Wrong: ${this.wrongCount}</strong><br>
+                        <button type="button" class="restart btn btn-outline-info btn-lg" data-dismiss="modal">Restart</button>
+                    </h4>
+                `);
+            },
+            restart(){
+                console.log("Restart");
             }
+            
         }
         trivia.init();
     });
